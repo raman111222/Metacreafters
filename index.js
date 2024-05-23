a9cd562eb5ccd8315f7ab519fa8b5ecc5bf4eeff
Ramandeepsingh45 <74469492+raman111222@users.noreply.github.com>
2024-05-23 21:01:56 +0530

    added the real thing

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const http = require("http");
-const url = require("url");
-const server = http.createServer((req, res) => {
-  const path = console.log(req.url);
-  if (path == "black") {
-    res.end("Hello nigger");
-  } else {
-    res.end("OOSKDOKSODK");
+let nfts = [];
+
+function mintNFT(name, description) {
+  let NFT = {
+    name: name,
+    description: description,
+  };
+  nfts.push(NFT);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs() {
+  for (let i = 0; i < nfts.length; i++) {
+    console.log("Nft number: " + (i + 1));
+    console.log("Name: " + nfts[i].name);
+    console.log("Description: " + nfts[i].description);
   }
-});
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Server started");
-});
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+  console.log("Number of NFTs: " + nfts.length);
+}
+
+// call your functions below this line
+mintNFT("MEow", "Just testing");
+mintNFT("apple", "second one");
+mintNFT("banan", "Third one");
+
+listNFTs();
+getTotalSupply();
